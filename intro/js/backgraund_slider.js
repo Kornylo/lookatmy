5e9bc87b84b541cbdb076b5aaaa4a901ee7b149e
kornylo <kornulo@gmail.com>
2016-02-22 15:23:18 +0000

    added new site

---
+++ intro/js/backgraund_slider.js
@@ -0,0 +1,17 @@
+var $ = jQuery;
+
+
+$.vegas('slideshow', {
+  delay:5000,
+  backgrounds:[
+    { src:'./intro/img/slide-0.jpg', fade:2000 },
+   { src:'./intro/img/slide-1.jpg', fade:2000 },
+    { src:'./intro/img/slide-2.jpg', fade:2000 },
+   { src:'./intro/img/slide-3.jpg', fade:2000 },
+    { src:'./intro/img/slide-4.jpg', fade:2000 },
+
+     
+  ]
+
+});
+
